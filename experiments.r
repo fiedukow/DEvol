204c3c36bfee18b9181ff77129488c994f5dfc92
Andrzej Fiedukowicz <root@ks391552.kimsufi.com>
2014-05-06 22:53:08 +0200

    Commiting empty experiments set.

--- experiments.r
+++ experiments.r
@@ -1,18 +1,3 @@
 source("multidim.r")
 
-setwd("~/repo/DEvol")
-runExperiment(experiment_name = "TEST2",
-              dims = 50,
-              range = c(-5,5),
-              pop_size = 100,
-              diff_factor = 1.0,
-              init_type = "unif",
-              select_type = "best",
-              crossover_type = "exp",
-              cr = 0.75,
-              qual = function(p) { cec2005benchmark(10, p) },
-              qual_description = "CEC 2005 #10",
-              best_possiblle = -330,
-              generations = 3,
-              near_enough = 0.001,
-              diff_size = 1)
+
