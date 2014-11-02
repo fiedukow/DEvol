a00eb557800399d2c967649f792ca430fa16d78c
fiedukow <fiedukow@gmail.com>
2014-11-02 15:53:53 +0100

    Added wilcox test for planed experiments.

--- multidim.r
+++ multidim.r
@@ -230,7 +230,7 @@ save_results = function(de_result) {
               col.names=F, row.names=F)
   # Maybe dump quality function values as well?
   write(de_result$values, file=paste("./results/", de_result$experiment_name, "_values.txt", sep=""))
-  write(de_result$values, file=paste("./results/", de_result$experiment_name, "_middle.txt", sep=""))
+  write(de_result$mid_values, file=paste("./results/", de_result$experiment_name, "_middle.txt", sep=""))
   system(paste("./gen_html_report.sh \"", de_result$experiment_name, "\"", sep=""))
   dump("de_result", file=paste("./results/", de_result$experiment_name, "_dump.r", sep=""))
 }
