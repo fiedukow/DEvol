2a7ccba5f53e45567856b363070624f951c99b86
fiedukow <fiedukow@gmail.com>
2014-12-15 19:18:45 +0100

    General library code fixes.
     -> Fixes in drawing popultion (2D) function.
     -> Mid point calculations fixup.
     -> Reversed Cr fixup.
     -> de loop cleanups.

--- multidim.r
+++ multidim.r
@@ -30,12 +30,12 @@ crossover_bin = function(x, y, cr) {
 }
 
 crossover_exp = function(x, y, cr) {
-  mod = t(replicate(nrow(x), cummin(runif(ncol(x)) > cr)))
+  mod = t(replicate(nrow(x), cummin(runif(ncol(x)) < cr)))
   mod*x + (1-mod)*y
 }
 
 crossover_exp_perm = function(x, y, cr) {
-  mod = t(replicate(nrow(x), cummin(runif(ncol(x)) > cr)))
+  mod = t(replicate(nrow(x), cummin(runif(ncol(x)) < cr)))
   mod = sample(mod)
   mod*x + (1-mod)*y
 }
@@ -65,35 +65,42 @@ range_fit_roll = function(p, range) {
 }
 
 # This is only valid for two demensions
-draw_population = function(pop, range, qual) {
-  # FIXME: This will not work after "Improving select_best_real to acctually use already calculated qual values."
-  if (length(pop[1,]) != 2) {
+draw_population = function(pop, pop_next, archive, mid, range, qual, no, name = "") {
+  if (length(pop[[1]][1,]) != 2) {
     warning("Draw population is undefined for object dimensions size other then 2")
     return(NA)
   }
-  x = seq(from = range[1], to = range[2], by = 0.01)
-  y = seq(from = range[1], to = range[2], by = 0.01)
+  png(file = paste0("img/", name, no, ".png"), width=800, height=800, bg = "white")
+  x = seq(from = range[1], to = range[2], by = 1)
+  y = seq(from = range[1], to = range[2], by = 1)
   contour(x, y, matrix(qual(as.matrix(expand.grid(x,y))),
                        nrow=length(x)))
-  points(pop, col="blue")
-  points(select_best(pop), col="red", lw=2)
+  points(archive, col="gray", lwd=1)
+  points(pop[[1]], col="blue", lwd=2, pch="O")
+  points(pop_next, col="purple", lwd=2)
+  points(select_best(pop, 2), col="red", lw=3)
+  points(mid, col="green", pch="x", lw=3)
+  dev.off()
 }
 
 de = function(dims, range, pop_size, diff_factor, init, select, crossover,
               cr, qual, generations, diff_size, range_fit = range_fit_mirror,
-              N_history = pop_size, noise_sd = 1, best_possible = NA, near_enough = NA) {
+              N_history = pop_size, noise_sd = 1, best_possible = NA, near_enough = NA, name="") {
   if (N_history < pop_size)
     stop("History must be at least 1 population long")
   if (N_history %% pop_size != 0)
     stop("For now de() is well defined only for N_history being multiple of pop_size")
 
+  last_candidats = matrix()
   pop_prev = list()
   pop_next = list()
   H_norm = list()
   pop_prev[[1]] = init(pop_size, dims, range)
   pop_prev[[2]] = qual(pop_prev[[1]])
   mean_pop_prev = mean(dist(pop_prev[[1]]))
-  H_norm[[1]] = (pop_prev[[1]] - matrix(colMeans(pop_prev[[1]]), nrow=pop_size, ncol=dims, byrow=TRUE)) / mean_pop_prev
+  col_means = colMeans(pop_prev[[1]])
+  col_means_matrix = matrix(col_means, nrow=pop_size, ncol=dims, byrow=TRUE)
+  H_norm[[1]] = (pop_prev[[1]] - col_means_matrix) / mean_pop_prev
   H_norm[[2]] = pop_prev[[2]]
   N_runif = pop_size*dims
 
@@ -106,43 +113,61 @@ de = function(dims, range, pop_size, diff_factor, init, select, crossover,
 
   begin = Sys.time()
   for(i in 1:generations) {
-    best_value = qual(pop_prev[[1]][which.best(pop_prev[[2]]), ])
-    result$values[i] = best_value
-    result$record_value = best(result$record_value, best_value)
-    this_mid_value = qual(colMeans(pop_prev[[1]]))
-    result$mid_values[i] = this_mid_value
-    result$record_mid_value = best(result$record_mid_value, this_mid_value)
+    ##### DETERMINE GENERATION WIDE VALUES
+    mid_point = matrix(colMeans(pop_prev[[1]]), ncol=dim)
+    H_unnorm = t(t(H_norm[[1]]*mean_pop_prev)+as.vector(mid_point))
 
+    ##### SAVING GENERATION DETAILS
+    result$values[i] = qual(pop_prev[[1]][which.best(pop_prev[[2]]), ])
+    result$record_value = best(result$record_value, result$values[i])
+    result$mid_values[i] = qual(mid_point)
+    result$record_mid_value = best(result$record_mid_value, result$mid_values[i])
+
+    ##### CHECKING IF STOP IS POSSIBLE (GOAL REACHED)
     if (!is.na(best_possible) && !is.na(near_enough) &&
-        abs(qual(best_value) - best_possible) < near_enough) {
+        abs(qual(result$values[i]) - best_possible) < near_enough) {
       print(paste("Found good enough in ", i, " generation."))
       break;
     }
-    pop_next[[1]] = select(pop_prev, pop_size) + mean_pop_prev*diff_factor*diff_vector(H_norm[[1]], pop_size)
-                    + rnorm(N_runif, mean = 20, sd = noise_sd)
+
+    ##### DRAWING GENERATION IF POSSIBLE
+    if (dims == 2)
+      draw_population(pop_prev, last_candidats, H_unnorm, mid_point, range, qual, i, name)
+
+    ##### GENERATING NEW POPULATION
+    pop_next[[1]] = select(pop_prev, pop_size)
+                    + diff_factor*diff_vector(H_unnorm, pop_size)
+                    + rnorm(N_runif, mean = 0, sd = noise_sd)
     pop_next[[1]] = crossover(pop_prev[[1]], pop_next[[1]], cr)
     pop_next_fit = range_fit(pop_next[[1]], range)
     pop_next[[2]] = qual(pop_next_fit)
+    last_candidats = pop_next[[1]]
 
+    ###### SELECTING BETTER FROM PAIRS
     # TODO better turnament then simple selecting better.
     mod = better(pop_prev[[2]], pop_next[[2]])
     pop_next[[1]] = pop_prev[[1]]*mod + pop_next[[1]]*(1-mod)
     pop_next_fit = pop_prev[[1]]*mod + pop_next_fit*(1-mod)
     pop_next[[2]] = pop_prev[[2]]*mod + pop_next[[2]]*(1-mod)
 
-    H_norm_next = pop_next[[1]] - matrix(colMeans(pop_next[[1]]), nrow=pop_size, ncol=dims, byrow=TRUE)
+    ###### UPDATING NORMALIZED ARCHIVE
+    H_norm_next = t(t(pop_next[[1]]) - as.vector(mid_point))
     mean_pop_next = mean(dist(pop_next[[1]]))
     H_norm[[1]] = rbind(H_norm[[1]],
                         H_norm_next/mean_pop_next)
-    H_norm[[2]] = pop_next[[2]]
+    H_norm[[2]] = rbind(H_norm[[2]],
+                        pop_next[[2]])
 
-    # Shift History window
+    ###### SHIFTING HISTORY WINDOW
     N = nrow(H_norm[[1]])
     H_norm[[1]] = H_norm[[1]][max(1, N - N_history + 1):N, ]
     H_norm[[2]] = H_norm[[2]][max(1, N - N_history + 1):N]
 
+    ###### NEXT LOOP PREPARATION
     pop_next[[1]] = pop_next_fit
     pop_prev = pop_next
+
+    ####### ITS HEAVY SO WE ARE AVOIDING CALCULATING IT AGAIN AT THE BEGINING OF THE LOOP
     mean_pop_prev = mean_pop_next
   }
   result$generation = length(result$values)
@@ -152,7 +177,7 @@ de = function(dims, range, pop_size, diff_factor, init, select, crossover,
   result$time_taken = as.numeric(Sys.time())-as.numeric(begin)
   result$H_norm = H_norm
   result$last_pop = pop_prev
-  result$middle = colMeans(pop_prev[[1]])
+  result$middle = mid_point
   result$middle_qual = qual(result$middle)
 
   return(result)
@@ -265,7 +290,8 @@ runExperiment = function(experiment_name, dims, range, pop_size, diff_factor, in
               N_history,
               noise_sd,
               best_possible,
-              near_enough);
+              near_enough,
+              name=experiment_name);
 
   # appending params to result to generate report from that
   result$select = select
